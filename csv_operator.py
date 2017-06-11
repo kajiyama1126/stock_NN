58000f185e680e7ab33d945d7fa1485b86709ade
kajiyama1126 <yuichi11261126@gmail.com>
2017-06-11 16:04:40 +0900

    First Commit

---
+++ csv_operator.py
@@ -0,0 +1,93 @@
+import os
+import shutil
+import codecs
+import pandas as pd
+import datetime
+import copy
+
+
+def copy_file_size_select(size, name, place, to_place):
+    os.chdir(place)
+    try:
+        os.makedirs(to_place)
+    except:
+        pass
+    if os.path.getsize(name) > size:
+        shutil.copyfile(place + '\\' + name, to_place + '\\' + name)
+
+def except0KB(place, to_place):#0Kbのファイルを除いてコピー(place⇨to_place)
+    directory = os.listdir(place)
+    for name in directory:
+        copy_file_size_select(100, name, place, to_place)
+
+def column_rename(place,to_place,add_name):
+    os.chdir(place)
+    directory = os.listdir(place)
+    for name in directory:
+        data = pd.read_csv(name,encoding='cp932')
+        print(data.columns)
+        data.columns = ["code",'type','market','start','high','low','last','number of sale', 'money of sale']
+        data.index =  data["code"]
+        data = data.drop("code", axis = 1)
+        data = data.dropna()
+        os.chdir(to_place)
+        rename = name.rstrip('.csv') + add_name+'.csv'
+        data.to_csv(rename)
+        os.chdir(place)
+
+def connect_some_days(days, place):#株価データを数日分結合
+    folder_name = 'stock_data_for' + str(days) + 'days'
+    os.chdir('/Users/kajiyama/PycharmProjects/stock_NN/stock_data')
+    try:
+        os.mkdir(folder_name)
+    except:
+        pass
+    directory = os.listdir(place)
+    os.chdir(place)
+    for i in range(len(directory)-days+1):
+        base_data = pd.DataFrame()
+        for j in range(days):
+            data = pd.read_csv(directory[i+j], encoding='cp932', index_col='code')
+            if j != 0:
+                data = data.drop(['type','market'], axis=1)
+                data.columns = ['start'+str(j), 'high'+str(j), 'low'+str(j), 'last'+str(j), 'number of sale'+str(j), 'money of sale'+str(j)]
+            base_data = pd.concat([base_data,data],axis=1)
+        base_data = base_data.dropna()
+        filename = directory[i].rstrip('.csv') + 'for' + str(days) + 'days.csv'
+        os.chdir('/Users/kajiyama/PycharmProjects/stock_NN/stock_data/'+folder_name)
+        base_data.to_csv(filename)
+        os.chdir(place)
+
+
+
+def base_transform(place):
+    basename = ['start','high','low','last']
+    os.chdir(place)
+    directory =os.listdir(place)
+    name = directory[0]
+    # for name in directory:
+    data = pd.read_csv(name, encoding='cp932')
+    # print(data)
+    start = copy.copy(data['start'].values)
+    # print(start)
+    for i in range(len(basename)):
+        data[basename[i]] = (data[basename[i]]/start -1.0)
+    print(data)
+
+
+
+
+
+place = '/Users/kajiyama/PycharmProjects/stock_NN/stock_data/download_stock_data_test'
+to_place = '/Users/kajiyama/PycharmProjects/stock_NN/stock_data/stock_data_except0KB'
+to_place2 = '/Users/kajiyama/PycharmProjects/stock_NN/stock_data/column_rename'
+to_place3 = '/Users/kajiyama/PycharmProjects/stock_NN/stock_data/stock_data_for2days'
+add = 'column_rename'
+
+
+# except0KB(place,to_place)
+
+# column_rename(to_place,to_place2,add)
+
+# connect_some_days(3,to_place2)
+base_transform(to_place3)
---
+++ downloader.py
@@ -0,0 +1,54 @@
+#-*- coding:utf-8 -*-
+import urllib.request
+import os
+import shutil
+from time import sleep
+import  sys
+
+def download(url,name,place):
+    urllib.request.urlretrieve(url, name+'.csv')
+
+def copy_file_size_select(size, name, place, to_place):
+    os.chdir(place)
+    try:
+        os.makedirs(to_place)
+    except:
+        pass
+    if os.path.getsize(name) > size:
+        shutil.copyfile(place + '\\' + name, to_place + '\\' + name)
+
+
+name = '2015-09-03'
+url = 'http://k-db.com/stocks/'+name+'?download=csv'
+place = 'stockdata'
+
+# years=['2015','2016','2017']
+years=['2010','2011','2012','2013','2014']
+months = [str(i+1).zfill(2) for i in range(12)]
+days = [str(i+1).zfill(2) for i in range(31)]
+print(months)
+# dates = []
+# for i in range()
+#
+place = 'Users/kajiyama/PycharmProjects/stock_NN/stock_data/download_stock_data'
+to_place = 'Users/kajiyama/PycharmProjects/stock_NN/stock_data/stock_data_except0KB'
+for year in years:
+    for month in months:
+        for day in days:
+            copy_file_size_select(1000,year+'-'+month+'-'+day+'.csv',place,to_place)
+
+# os.chdir(place)
+# for year in years:
+#     for month in months:
+#         for day in days:
+#             for i in range(10):
+#                 try:
+#                     name = year+'-'+month+'-'+day
+#                     url = 'http://k-db.com/stocks/' + name + '?download=csv'
+#                     print(name)
+#                     sleep(10)
+#                     download(url, name, place)
+#                     break
+#                 except urllib.error.HTTPError:
+#                     print('HTTPError')
+#                     sleep(120)
